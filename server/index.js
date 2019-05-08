a7f59c279a29fa897c40210c900e45aa43cefc7b
Yerko Palma <yerko.palma@usach.cl>
2019-05-08 15:55:20 -0400

    handle redis operations

--- server/index.js
+++ server/index.js
@@ -1,29 +1,49 @@
 const http = require('http')
 const Router = require('./lib/router')
 const { json } = require('./lib/body')
+const redis = require('redis')
+const { promisify } = require('util')
+const client = redis.createClient()
+
+const hmset = promisify(client.hmset).bind(client)
+const hgetall = promisify(client.hgetall).bind(client)
+const set = promisify(client.set).bind(client)
+const get = promisify(client.get).bind(client)
 
 const router = new Router()
 
-router.get('/api/ciudades', (req, res) => {
+router.get('/api/ciudades', async (req, res) => {
   // obtiene lista de ciudades desde redis
   // por cada ciudad
   //  obtiene datos de ciudad desde forecast.io
   // retorna arreglo con info de ciudades
-  console.log('llamada a /api/ciudades')
-  res.end()
+  const allKeys = (await get('ciudades')).split(',').filter(key => key !== 'api.errors')
+  const ciudades = await Promise.all(allKeys.map(async key => {
+    return Object.assign({}, { codigo: key }, await hgetall(key))
+  }))
+  res.end(JSON.stringify(ciudades))
 })
-router.get('/api/ciudades/:ciudad', (req, res, params) => {
+router.get('/api/ciudades/:ciudad', async (req, res, params) => {
   // busca :ciudad en redis
   // obtiene datos de ciudad en forecast.io
   // retorna datos de ciudad
-  console.log('llamada a /api/ciudades/' + params.ciudad)
-  res.end()
+  const ciudad = await hgetall(params.ciudad)
+  res.end(JSON.stringify(Object.assign({}, { codigo: params.ciudad }, ciudad)))
 })
 router.post('/api/ciudades', async (req, res) => {
   // agrega datos de ciudad a redis
-  // const ciudades = await json(req)
-  console.log('llamada a POST /api/ciudades')
-  res.end()
+  const ciudad = await json(req)
+  const key = ciudad.codigo
+  delete ciudad.codigo
+  // guardo el objeto de ciudad
+  await hmset(key, Object.entries(ciudad).flat())
+  // para busquedas posteriores
+  let ciudades = (await get('ciudades')).split() || []
+  if (ciudades.indexOf(key) < 0) {
+    ciudades.push(key)
+    await set('ciudades', ciudades.join())
+  }
+  res.end(JSON.stringify(ciudades))
 })
 
 const server = http.createServer()
--- server/lib/body.js
+++ server/lib/body.js
@@ -1,19 +1,21 @@
+const { promisify } = require('util')
 exports.body = body
 exports.json = json
 
-function body (req) {
+function body (req, cb) {
   let body = []
   req.on('data', chunk => {
     body.push(chunk)
   })
   req.on('end', () => {
     body = Buffer.concat(body)
-    return Promise.resolve(body)
+    cb(null, body)
   })
-  req.on('error', err => Promise.reject(err))
+  req.on('error', err => cb(err))
 }
 
 async function json (req) {
-  const value = await body(req)
+  const bodyPromise = promisify(body)
+  const value = await bodyPromise(req)
   return JSON.parse(value.toString())
 }
