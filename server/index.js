90caf7f7b81861088bc9e5479383d9df42ca0e93
Yerko Palma <yerko.palma@usach.cl>
2019-05-09 14:37:03 -0400

    try to get data from forecast service

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const http = require('http')
 const Router = require('./lib/router')
 const Controller = require('./lib/controller')
-const { send, sendError, json } = require('./lib/server')
+const { send, sendError, json, request } = require('./lib/server')
 const redis = require('redis')
 const client = redis.createClient()
 const controller = new Controller(client)
 const initialData = require('./init.json')
 
 const router = new Router()
+const FORECAST_URL = `https://api.darksky.net/forecast/${process.env.API_KEY}`
 
 router.get('/api/ciudades', async function handler (req, res) {
   if (Math.random(0, 1) < 0.1) {
@@ -25,11 +26,14 @@ router.get('/api/ciudades', async function handler (req, res) {
   }
   // obtiene lista de ciudades desde redis
   const ciudades = await controller.loadAll('ciudades')
-  // TODO:
   // por cada ciudad
-  //  obtiene datos de ciudad desde forecast.io
-  // retorna arreglo con info de ciudades
-  send(res, ciudades) // res.end(JSON.stringify(ciudades))
+  const data = ciudades.map(async ciudad => {
+    const { currently } = await request(`${FORECAST_URL}/${ciudad.lat},${ciudad.lang}?units=si`)
+    // obtiene datos de ciudad desde forecast.io
+    return Object.assign({}, ciudad, { time: new Date(currently.time * 1000), temperature: currently.temperature })
+  })
+
+  send(res, data)
 })
 router.get('/api/ciudades/:ciudad', async function handler (req, res, params) {
   if (Math.random(0, 1) < 0.1) {
@@ -47,10 +51,10 @@ router.get('/api/ciudades/:ciudad', async function handler (req, res, params) {
   }
   // busca :ciudad en redis
   const ciudad = await controller.load(params.ciudad)
-  // TODO:
   // obtiene datos de ciudad en forecast.io
+  const { currently } = request(`${FORECAST_URL}/${ciudad.lat},${ciudad.lang}?units=si`)
   // retorna datos de ciudad
-  send(res, Object.assign({}, { codigo: params.ciudad }, ciudad)) // res.end(JSON.stringify(Object.assign({}, { codigo: params.ciudad }, ciudad)))
+  send(res, Object.assign({}, { codigo: params.ciudad }, ciudad, { time: new Date(currently.time * 1000), temperature: currently.temperature }))
 })
 router.post('/api/ciudades', async function handler (req, res) {
   if (Math.random(0, 1) < 0.1) {
