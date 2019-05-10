bc821dde91f7f63822f9e2ad6455be82ee22d776
Yerko Palma <yerko.palma@usach.cl>
2019-05-10 03:18:38 -0400

    use heroku port

--- server/index.js
+++ server/index.js
@@ -83,8 +83,8 @@ server.on('request', (req, res) => {
   router.lookup(req, res)
 })
 
-server.listen(3000, async () => {
-  console.log('listening on port 3000')
+server.listen(process.env.PORT || 3000, async () => {
+  console.log('server ready')
   // guardar en redis información de ciudades iniciales
   await controller.saveAll('ciudades', initialData)
 })
