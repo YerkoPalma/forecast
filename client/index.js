96810a78b7f09fc33da377de855ad954d091ae4a
Yerko Palma <yerko.palma@usach.cl>
2019-05-10 01:19:54 -0400

    added react for front-end

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+
+const rootEl = document.getElementById('app')
+
+render(<App />, rootEl)
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract(
+        {
+          fallback: 'style-loader',
+          use: ['css-loader']
+        }
+      )
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader'
+        }
+      ]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      hash: true,
+      filename: 'index.html', // target html
+      template: './client/index.html' // source html
+    }),
+    new ExtractTextPlugin({ filename: 'css/style.css' })
+  ]
+}
